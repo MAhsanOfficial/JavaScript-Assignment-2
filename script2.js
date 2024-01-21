d04e159a278ac108c49f484dcbf27bfb0cb99a47
MAhsanOfficial <155831296+MAhsanOfficial@users.noreply.github.com>
2024-01-21 01:18:15 -0800

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,12 @@
+alert('Asalam Walikum')
+
+let userName = prompt('Whats Your Name?')
+alert(  "" +  userName +"..." + 'Welcome to This My WebPage' )
+
+var length = prompt('Length of Rectangle')
+var width = prompt('Width of Rectangle')
+
+let area = (length*width)
+console.log('This Area Of Rectangle is ' + ""+area);   
+
+ 
